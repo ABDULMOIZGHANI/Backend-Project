b5e236e40104c982c2c123fc8c2355c9cec39fdc
ABDUL MOIZ <babamoiz289@gmail.com>
2025-01-18 15:35:16 +0500

    Utils Files To Add or Make a Same Fucntion

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+); // when we use "app.use" it means it is a middleware
+
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+export { app };
